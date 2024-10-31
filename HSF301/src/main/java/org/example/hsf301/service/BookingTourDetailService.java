8d76a42706546217582d0a3b37a74dc3d40a5f9c
Nguyễn Hoàng Đức Minh <minhnhdse181549@fpt.edu.vn>
2024-11-01 02:29:17 +0700

    small change

--- HSF301/src/main/java/org/example/hsf301/service/BookingTourDetailService.java
+++ HSF301/src/main/java/org/example/hsf301/service/BookingTourDetailService.java
@@ -40,21 +40,50 @@ public class BookingTourDetailService implements IBookingTourDetailService {
 
     @Override
     public BookingTourDetail updateBookingTourDetail(Long bookingTourDetailId, BookingTourDetailRequest bookingTourDetailRequest) throws Exception {
-        return null;
+        BookingTourDetail bookingTourDetail = bookingTourDetailRepository.findById(bookingTourDetailId);
+        if(bookingTourDetail == null) {
+            throw new IllegalArgumentException("bookingTourDetail cannot be null");
+        }
+        Bookings bookings = bookingRepository.findById(bookingTourDetailRequest.getBookingID());
+        if(bookings == null) {
+            throw new IllegalArgumentException("bookings cannot be null");
+        }
+        Tours tours = tourRepository.findById(bookingTourDetailRequest.getTourID());
+        if(tours == null) {
+            throw new IllegalArgumentException("tours cannot be null");
+        }
+        bookingTourDetail.setParticipant(bookingTourDetailRequest.getParticipant());
+        bookingTourDetail.setTourId(tours);
+        bookingTourDetail.setBooking(bookings);
+        bookingTourDetailRepository.update(bookingTourDetail);
+        return bookingTourDetail;
     }
 
     @Override
     public void deleteBookingTourDetail(Long bookingTourDetailId) {
+        Bookings bookings = bookingRepository.findById(bookingTourDetailId);
+        if(bookings == null) {
+            throw new IllegalArgumentException("bookings cannot be null");
+        }
+        bookingTourDetailRepository.delete(bookingTourDetailId);
 
     }
 
     @Override
     public BookingTourDetail getBookingTourDetail(Long bookingTourDetailId) {
-        return null;
+      BookingTourDetail bookingTourDetail = bookingTourDetailRepository.findById(bookingTourDetailId);
+      if(bookingTourDetail == null) {
+          throw new IllegalArgumentException("bookingTourDetail cannot be null");
+      }
+      return bookingTourDetail;
     }
 
     @Override
     public List<BookingTourDetail> getAllBookingTourDetails() {
-        return List.of();
+        List<BookingTourDetail> bookingTourDetails = bookingTourDetailRepository.getAll();
+        if(bookingTourDetails == null) {
+            throw new IllegalArgumentException("bookingTourDetails cannot be null");
+        }
+        return bookingTourDetails;
     }
 }
